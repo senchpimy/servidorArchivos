15d83e1a2949b9601d2288416a6b88ae82143c1d
senchi <senchpimy@gmail.com>
2023-01-16 13:17:43 -0600

    handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::response::Redirect;
+#[macro_use] extern crate rocket;
+
+#[get("/<_..>")]
+fn everything() -> Redirect{
+        Redirect::to("/")
 }
+
+#[get("/")]
+fn papu()-> String{
+"hola mundo".to_string()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![papu]).mount("/", routes![everything])
+    //rocket::build().mount("/", routes![hello])
+}
+
