4414a388e350d9233bb997cb2fb5a7533d9b1841
senchi <senchpimy@gmail.com>
2023-01-16 14:55:08 -0600

    getting ifo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use rocket::response::Redirect;
+use rocket::config::Config;
 use rocket::fs::NamedFile;
+use rocket::serde::{Deserialize, json};
 #[macro_use] extern crate rocket;
+
+#[derive(Debug, PartialEq, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Data<'r> {
+    ip: &'r str,
+    password: &'r str,
+    stars: usize,
+}
 
 #[get("/<_..>")]
 fn everything() -> Redirect{
@@ -12,7 +22,20 @@ async fn index() -> Option<NamedFile> {
     NamedFile::open("index.html").await.ok()
 }
 
+#[post("/",data="<input>")]
+fn test(input:&str)->&'static str{
+    println!("papu");
+    println!("{}",input);
+let data: Data = json::from_str(input).unwrap();
+if (data==Data { ip: "Rocket",password: "Rocket", stars: 5, }){
+    return "hola"
+}
+"No hola"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index]).mount("/", routes![everything])
+    let config = Config::default();
+    rocket::custom(config).mount("/", routes![index]).mount("/", routes![everything])
+                          .mount("/",routes![test])
 }
